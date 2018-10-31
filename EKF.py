1ff57092b891e052829b18989727f3f877bb2611
zyc <yunchu@g.ucla.edu>
2018-10-30 21:57:32 -0700

    change3

--- EKF.py
+++ EKF.py
@@ -134,7 +134,9 @@ class EKF:
 
 	def q_inverse(self,q):
 		q.x, q.y, q.z=-q.x, -q.y, -q.z
-		return q=q/math.sqrt(q.w**2+q.x**2+q.y**2+q.z**2)
+		 
+		q=q/math.sqrt(q.w**2+q.x**2+q.y**2+q.z**2)
+		return q
 
 	def q2array(self, q):
 		return a=np.array([q.w,q.x,q.y,q.z])
