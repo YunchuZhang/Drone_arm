8cdcde4647e7bc766a8a605f4a1ba09405f18b1d
Zhi29 <zhili5494@gmail.com>
2018-11-01 04:00:51 -0700

    test

--- EKF.py
+++ EKF.py
@@ -141,6 +141,7 @@ class EKF:
 		self.K = self.P*self.H.T*(self.H*self.P*self.H.T+self.R).I
 		print "self.K: ",self.K
 		print "z-zhat: ", z.T-self.zhat.T
+		print "slef.x.T: ", self.x.T
 		self.x.T = self.x.T+self.K*(z.T-self.zhat.T)
 		print "z-zhat: ", z-self.zhat
 		I=np.mat(np.eye(16))
