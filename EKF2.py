d0470ce58ceafafa13a6ae588ccd51dda7666d88
Zhi29 <zhili5494@gmail.com>
2018-11-07 09:32:55 -0800

    add a state

--- EKF2.py
+++ EKF2.py
@@ -10,15 +10,15 @@ GRAVITY=np.array([0,0,-9.8])
 mpl = mpl()
 
 class EKF:
-	x=np.zeros(10)#10 states q p v bw ba
-	xdot=np.zeros(10)#10 states derivaties
+	x=np.zeros(13)#10 states q p v bw ba
+	xdot=np.zeros(13)#10 states derivaties
 	z=np.zeros(6)#real raw data from sensor
 	zhat=np.zeros(6)#H*x_bar
-	P=np.eye(10)#covariance matrix
-	Q=np.zeros([6,6])#process noise covariance
-	F=np.zeros((10,10))#state transition
-	G=np.zeros((10,6))
-	H=np.zeros((6,10))#observation Matrix
+	P=np.eye(13)#covariance matrix
+	Q=np.zeros([9,9])#process noise covariance
+	F=np.zeros((13,13))#state transition
+	G=np.zeros((13,9))
+	H=np.zeros((6,13))#observation Matrix
 	R=np.eye(6)#observation noise Matrix
 	gyro_cov=0.0025
 	acc_cov = 0.01
@@ -42,6 +42,7 @@ class EKF:
 		self.x[0]=1
 		self.Q[0:3,0:3] = np.eye(3)*self.gyro_cov
 		self.Q[3:6,3:6] = np.eye(3)*self.bw_cov
+		self.Q[6:9,6:9] = np.eye(3)*self.ba_cov
 		self.R[0:3,0:3] *= self.gravity_cov
 		self.R[3:6,3:6] *= self.gyro_cov
 
@@ -73,9 +74,11 @@ class EKF:
 		#print "x_qian: ", self.x[10:16]
 		self.x[0:4] += self.xdot[0:4]*dt
 		self.x[7:10] += self.xdot[7:10]*dt
+		self.x[10:13] += self.xdot[10:13]*dt
 		#print "x_hou: ", self.x[10:16]
 		self.F[0:4,0:4]=np.eye(4)+self.F[0:4,0:4]*dt
 		self.F[7:10,7:10]=np.eye(3)+self.F[7:10,7:10]*dt
+		self.F[10:13,10:13]=np.eye(3)+self.F[10:13,10:13]*dt
 		#self.G=self.G*dt
 		self.P=np.dot(np.dot(self.F,self.P),self.F.transpose())+\
 		np.dot(np.dot(self.G,self.Q),self.G.transpose())
@@ -110,16 +113,19 @@ class EKF:
 		self.x[4:7] = gyro_q[1:4]
 	
 		self.xdot[7:10] = -self.lamda*self.x[7:10]
+		self.xdot[10:13] = -self.lamda*self.x[10:13]
 
 		self.F[0:4,0:4] = 0.5*mpl.diff_pq_p(gyro_q)
 		self.F[0:4,4:7] = 0.5*mpl.diff_pq_q(q)[0:4,1:4]
 		self.F[4:7,7:10] = -np.eye(3)
 		self.F[7:10,7:10] = -self.lamda*np.eye(3)
+		self.F[10:13,10:13] = -self.lamda*np.eye(3)
 
 		#self.G[0:4,0:3] = -0.5*mpl.diff_pq_q(q)[0:4,1:4]
 		#self.G[4:7,3:6] = np.eye(3)
 		self.G[4:7,0:3] = np.eye(3)
 		self.G[7:10,3:6] = np.eye(3)
+		self.G[10:13,6:9] = np.eye(3)
 
 
 	def update(self, acc, gyro, t):#acc is the raw data from IMU
@@ -152,7 +158,7 @@ class EKF:
 		g_n_q=np.array([0.0,0.0,0.0,-1.0])
 		acc_q=mpl.q_p(mpl.q_p(q,g_n_q),self.q_inverse(q)) #????????normalize
 		print "acc_q: ", acc_q
-		self.zhat[0:3] = acc_q[1:4]
+		self.zhat[0:3] = acc_q[1:4]+self.x[10:13]
 		self.zhat[3:6] = self.x[4:7]+self.x[7:10]#wb+bw
 		self.H[0:3,0:4] = mpl.diff_qvqstar_q(q, GRAVITY)
 		self.H[3:6,4:7] = np.eye(3)
