84bc2ddc0a3639d287da518ee31efdb609c8d5e0
Zhi29 <zhili5494@gmail.com>
2018-11-12 12:58:13 -0800

    debug

--- EKF2.py
+++ EKF2.py
@@ -168,7 +168,7 @@ class EKF:
 		#mag = np.dot(Rotation_mat,mag)
 
 		z=np.zeros(6)
-		z[0:3]=acc#/np.linalg.norm(acc,ord=2)
+		z[0:3]=acc/np.linalg.norm(acc,ord=2)
 		z[3:6]=gyro
 		#z[6:9]=mag
 		self.measurement()
@@ -189,12 +189,12 @@ class EKF:
 		q=np.array([0.0,0.0,0.0,0.0])
 		q=self.x[0:4]
 		#ba=self.x[13:16]
-		g_n_q = np.array([0.0,0.0,0.0,-9.8])
+		g_n_q = np.array([0.0,0.0,0.0,1.0])
 		acc_q = mpl.q_p(mpl.q_p(q,g_n_q),self.q_inverse(q)) #????????normalize
 		geo_mag_field_local_var=np.array([0.0,geo_mag_field[0],geo_mag_field[1],geo_mag_field[2]])
 		mag_zhat = mpl.q_p(mpl.q_p(q,geo_mag_field_local_var),self.q_inverse(q))
 		print ("acc_q: ", acc_q)
-		self.zhat[0:3] = acc_q[1:4]+self.x[10:13]#acc+ba
+		self.zhat[0:3] = acc_q[1:4]+self.x[10:13]/np.linalg.norm(self.x[10:13],ord=2) #acc+ba
 		self.zhat[3:6] = self.x[4:7]+self.x[7:10]#wb+bw
 		#self.zhat[6:9] = mag_zhat[1:4]+self.x[13:16]#mag+bm
 		self.H[0:3,0:4] = mpl.diff_qvqstar_q(q, GRAVITY)
