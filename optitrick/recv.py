91c351ec5cebcad74b7f7ade37b1422a53d057be
Yunchu <33641269+YunchuZhang@users.noreply.github.com>
2018-11-13 17:30:32 -0800

    Add files via upload

--- optitrick/recv.py
+++ optitrick/recv.py
@@ -1,4 +1,4 @@
-import cv2
+import time
 import zmq
 import json
 import numpy as np
@@ -6,7 +6,8 @@ sub_port = 5556
 context = zmq.Context()
 #connect to socket we subscrib
 socket_sub = context.socket(zmq.SUB)
-socket_sub.connect("tcp://localhost:5556")
+#socket_sub.connect("tcp://localhost:5556")
+socket_sub.connect("tcp://192.168.1.10:5566")
 #socket_sub.setsockopt(zmq.SUBSCRIBE, b"")
 socket_sub.setsockopt(zmq.SUBSCRIBE,b'')
 def recv_array(socket, flags=0, copy=True, track=False):
@@ -17,9 +18,12 @@ def recv_array(socket, flags=0, copy=True, track=False):
     A = np.frombuffer(buf, dtype=md['dtype'])
     return A.reshape(md['shape'])
 while True:
+    start = time.clock()
     print('asds')
     contents  =  recv_array(socket_sub,copy=False)
     print(contents)
+    elapsed = (time.clock() - start)
+    print("time",elapsed)    
     #nparr = np.asarray(bytearray(contents), dtype="uint8")
 
 
