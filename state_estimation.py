04a6262b6a15f557b244b36ee262eb8fd144e80d
Zhi29 <zhili5494@gmail.com>
2018-11-15 14:38:20 -0800

    subtract offset of the height

--- state_estimation.py
+++ state_estimation.py
@@ -58,10 +58,10 @@ while True:
 	print('asds')
 	contents  =  recv_array(socket_sub,copy=False)
 	position = contents[0:3]
-	position[1] -= 0.22
 	
 	Rotation_mat=np.dot(np.array([[0,0,1],[0,1,0],[-1,0,0]]),np.array([[0,1,0],[-1,0,0],[0,0,1.0]]))
 	position = np.dot(Rotation_mat, position)
+	position[2] += 0.22
 
 	savet = (contents[0],contents[1],contents[2])
 	savepose.appendleft(savet)
